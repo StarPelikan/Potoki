6aedc95204eeaf5040c2ecce95185e596c101d67
StarPelikan <cccp-89@bk.ru>
2022-12-01 11:31:36 +0300

    potoki 1

---
+++ src/Basket.java
@@ -0,0 +1,84 @@
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+
+public class Basket {
+    protected String[] fruitName;
+    protected int[] price;
+    protected int[] fruitAmount;
+
+
+    public Basket(String[] fruitNumber, int[] price) {
+        this.fruitName = fruitNumber;
+        this.price = price;
+        this.fruitAmount = new int[fruitNumber.length];
+    }
+
+    public Basket(String[] fruitNumber, int[] price, int[] fruitAmount) {
+        this.fruitName = fruitNumber;
+        this.price = price;
+        this.fruitAmount = fruitAmount;
+    }
+
+    public void addToCart(int fruitNumber, int amount) {
+        fruitAmount[fruitNumber] += amount;
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+        int sumFruit = 0;
+        for (int i = 0; i < fruitName.length; i++) {
+            if (fruitAmount[i] != 0) {
+                System.out.println(fruitName[i] + " " + fruitAmount[i] + " шт., " + price[i] + " руб., " + (fruitAmount[i] * price[i]) + " рублей в сумме.");
+                sumFruit += fruitAmount[i] * price[i];
+            }
+        }
+        System.out.println("Итого в корзине: " + sumFruit + " рублей.");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String st : fruitName) {
+                out.write(st + ",");
+            }
+            out.write("\n");
+            for (int pr : price) {
+                out.write(pr + ",");
+            }
+            out.write("\n");
+            for (int amount : fruitAmount) {
+                out.write(amount + ",");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
+            String fruitName = br.readLine();
+            String price = br.readLine();
+            String fruitAmount = br.readLine();
+
+            String[] fruitStr = fruitName.split(",");
+
+            String[] priceStr = price.split(",");
+            int[] pricesInt = new int[priceStr.length];
+            for (int i = 0; i < pricesInt.length; i++) {
+                pricesInt[i] = Integer.parseInt(priceStr[i]);
+            }
+
+            String[] amountStr = fruitAmount.split(",");
+            int[] amountInt = new int[amountStr.length];
+            for (int i = 0; i < amountInt.length; i++) {
+                amountInt[i] = Integer.parseInt(amountStr[i]);
+            }
+            return new Basket(fruitStr, pricesInt, amountInt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String[] fruits = {"Яблоки", "Груши", "Персик"};
+        int[] prices = {85, 100, 200};
+        int fruitNumber = 0;
+        int productCount = 0;
+
+
+        Basket basket = new Basket(fruits, prices);
+        File basketFile = new File("basket.txt");
+        if (basketFile.exists()) {
+            System.out.println("загрузить вашу корзину?");
+            scanner.nextLine().equals("");
+            basket = Basket.loadFromTxtFile(basketFile);
+            basket.printCart();
+            System.out.println(" ");
+        } else {
+            basket = new Basket(fruits, prices);
+        }
+
+
+        System.out.println("Свеживы Завоз Фруктов, Налетай забирай дорогой ");
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println((i + 1) + ". " + fruits[i] + " " + prices[i] + " руб. за 1 кг.");
+        }
+
+        while (true) {
+
+            System.out.println("Выберай, дорогой, фрукты сколько кг тебе  или end ");
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                System.out.println("Уже уходишь? Заходи приходи, сладкий фрукт у меня ещё купи.");
+                break;
+            }
+            String[] parts = input.split(" ");
+            if (parts.length != 2) {
+                System.out.println("Брат ты ошибся с выбором.");
+                continue;
+            }
+            String y = parts[1];
+            String x = parts[0];
+            try {
+                fruitNumber = Integer.parseInt(x) - 1;
+                productCount = Integer.parseInt(y);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: Вы ввели не число. Попробуй еще");
+                continue;
+            }
+            if (fruitNumber + 1 > fruits.length || fruitNumber < 0) {
+                System.out.println("Ошибка, слишком большое число или отрицательное");
+                continue;
+            }
+            if (productCount < 0) {
+                System.out.println("ЭЭЭ, товарищ количество продуктов не может быть отрицательным.");
+                continue;
+            }
+            basket.addToCart(fruitNumber, productCount);
+            basket.saveTxt(basketFile);
+            basket.printCart();
+
+        }
+
+    }
+}
