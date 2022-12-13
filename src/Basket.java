34d81de2d5a31a07728975a1926c1cbe3309a8ef
StarPelikan <cccp-89@bk.ru>
2022-12-13 14:46:03 +0300

    json2

--- src/Basket.java
+++ src/Basket.java
@@ -1,9 +1,8 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.text.ParseException;
+
 import java.io.*;
 
-import static java.lang.Integer.parseInt;
 
 public class Basket {
     protected String[] fruitName;
@@ -38,6 +37,8 @@ public class Basket {
         }
         System.out.println("Итого в корзине: " + sumFruit + " рублей.");
     }
+
+    //В json
     public void saveJson(File textFile) throws IOException {
         FileWriter writer = new FileWriter(textFile);
         GsonBuilder builder = new GsonBuilder();
@@ -46,55 +47,53 @@ public class Basket {
         writer.close();
     }
 
-    public static Basket loadFromJson (File textFile) throws FileNotFoundException{
+    public static Basket loadFromJson(File textFile) throws IOException {
         Gson gson = new Gson();
         FileReader reader = new FileReader(textFile);
         return gson.fromJson(reader, Basket.class);
     }
 
-//    public void saveTxt(File textFile) throws IOException {
-//        try (PrintWriter out = new PrintWriter(textFile)) {
-//            for (String st : fruitName) {
-//                out.write(st + ",");
-//            }
-//            out.write("\n");
-//            for (int pr : price) {
-//                out.write(pr + ",");
-//            }
-//            out.write("\n");
-//            for (int amount : fruitAmount) {
-//                out.write(amount + ",");
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public static Basket loadFromTxtFile(File textFile) {
-//        try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
-//            String fruitName = br.readLine();
-//            String price = br.readLine();
-//            String fruitAmount = br.readLine();
-//
-//            String[] fruitStr = fruitName.split(",");
-//
-//            String[] priceStr = price.split(",");
-//            int[] pricesInt = new int[priceStr.length];
-//            for (int i = 0; i < pricesInt.length; i++) {
-//                pricesInt[i] = Integer.parseInt(priceStr[i]);
-//            }
-//
-//            String[] amountStr = fruitAmount.split(",");
-//            int[] amountInt = new int[amountStr.length];
-//            for (int i = 0; i < amountInt.length; i++) {
-//                amountInt[i] = Integer.parseInt(amountStr[i]);
-//            }
-//            return new Basket(fruitStr, pricesInt, amountInt);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    //В Text
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String st : fruitName) {
+                out.write(st + ",");
+            }
+            out.write("\n");
+            for (int pr : price) {
+                out.write(pr + ",");
+            }
+            out.write("\n");
+            for (int amount : fruitAmount) {
+                out.write(amount + ",");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
+            String fruitName = br.readLine();
+            String price = br.readLine();
+            String fruitAmount = br.readLine();
+
+            String[] fruitStr = fruitName.split(",");
+
+            String[] priceStr = price.split(",");
+            int[] pricesInt = new int[priceStr.length];
+            for (int i = 0; i < pricesInt.length; i++) {
+                pricesInt[i] = Integer.parseInt(priceStr[i]);
+            }
+
+            String[] amountStr = fruitAmount.split(",");
+            int[] amountInt = new int[amountStr.length];
+            for (int i = 0; i < amountInt.length; i++) {
+                amountInt[i] = Integer.parseInt(amountStr[i]);
+            }
+            return new Basket(fruitStr, pricesInt, amountInt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,89 @@
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String[] fruits = {"Яблоки", "Груши", "Персик"};
         int[] prices = {85, 100, 200};
         int fruitNumber = 0;
         int productCount = 0;
 
+// Херабора  что вычитал, чтение файла
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("shop.xml");
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        boolean isLoadEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/load/enabled")
+                .evaluate(doc));
+        String loadFileName = xPath
+                .compile("/config/load/fileName")
+                .evaluate(doc);
+        String loadFormat = xPath
+                .compile("/config/load/format")
+                .evaluate(doc);
+        boolean isSaveEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/save/enabled")
+                .evaluate(doc));
+        String saveFileName = xPath
+                .compile("/config/save/fileName")
+                .evaluate(doc);
+        String saveFormat = xPath
+                .compile("/config/save/format")
+                .evaluate(doc);
+        boolean isLogEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/log/enabled")
+                .evaluate(doc));
+        String logFileName = xPath
+                .compile("/config/load/fileName")
+                .evaluate(doc);
+
 
         Basket basket = new Basket(fruits, prices);
-        File logFile = new File("log.csv");
-        File basketFile = new File("basket.bin");
+        File log = new File("client.csv");
+        File textFile = new File("basket.bin");
         File jsonFile = new File("basket.json");
 
         ClientLog clientLog = new ClientLog();
 
-        if (jsonFile.exists()) {
-            System.out.println("загрузить вашу корзину?");
-            scanner.nextLine().equals("");
-            basket = Basket.loadFromJson(jsonFile);
-            basket.printCart();
-            System.out.println(" ");
+//Загрузка из файла с помощью хераборы
+//Разобрался почему не заходит в файл ... в настройках false, то бишь несмотреть, а создавайть новый....
+        if (isLoadEnabled) {
+            switch (loadFormat) {
+                case "json":
+                    basket.loadFromJson(new File(loadFileName));
+                    System.out.println("загрузить вашу корзину из формата json ?");
+                    scanner.nextLine().equals("");
+                    basket = Basket.loadFromJson(jsonFile);
+                    basket.printCart();
+                    break;
+                case "text":
+                    basket.loadFromTxtFile(new File(loadFileName));
+                    scanner.nextLine().equals("");
+                    basket = Basket.loadFromTxtFile(textFile);
+                    basket.printCart();
+                    System.out.println(" ");
+                    break;
+            }
         } else {
             basket = new Basket(fruits, prices);
         }
 
 
+//Вывод на экран и проверка
         System.out.println("Свеживы Завоз Фруктов, Налетай забирай дорогой ");
         for (int i = 0; i < fruits.length; i++) {
             System.out.println((i + 1) + ". " + fruits[i] + " " + prices[i] + " руб. за 1 кг.");
         }
 
         while (true) {
-
             System.out.println("Выберай, дорогой, фрукты сколько кг тебе  или end ");
             String input = scanner.nextLine();
             if ("end".equals(input)) {
@@ -68,13 +115,25 @@ public class Main {
 
 
             basket.addToCart(fruitNumber, productCount);
-            basket.saveJson(jsonFile);
+            //     Сохранение у формате через херабору
+            if (isSaveEnabled) {
+                switch (saveFormat) {
+                    case "json":
+                        basket.saveJson(new File(saveFileName));
+                        break;
+                    case "text":
+                        basket.saveTxt(new File(saveFileName));
+                        break;
+                }
+            }
+
             clientLog.log(fruitNumber, productCount);
-            clientLog.exportAsCSV(logFile);
+            if (isLogEnabled) {
+                clientLog.exportAsCSV(log);
+            }
             basket.printCart();
-
         }
-
     }
 
 }
+
